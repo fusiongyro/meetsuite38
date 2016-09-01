7bca4b1a03a8faca73f758133b7f5cbdb22ed109
Daniel Lyons <dlyons@nrao.edu>
2016-09-01 14:50:29 -0600

    messing around with a panel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,14 @@
 <template>
   <p>This is the home page.</p>
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h1>This is some panel content</h1>
+    </div>
+    <div class="panel-body">
+      <p>And here's more stuff inside the panel</p>
+    </div>
+  </div>
 </template>
 <style>
 </style>
